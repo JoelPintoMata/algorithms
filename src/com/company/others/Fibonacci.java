adec1a0d70454abaf9814c561cbcb2b78b351f31
joelpintodamata <G1thub@2012>
2018-08-05 12:00:28 +0200

    Code refactoring

--- src/com/company/others/Fibonacci.java
+++ src/com/company/others/Fibonacci.java
@@ -1,5 +1,7 @@
 package com.company.others;
 
+import java.math.BigInteger;
+
 /**
  * In mathematics, the Fibonacci numbers or Fibonacci series or Fibonacci sequence are the numbers in the following integer sequence:
  * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144...
@@ -7,16 +9,64 @@ package com.company.others;
  * and each subsequent number is the sum of the previous two. Below example shows how to create fibonacci series.
  */
 public class Fibonacci {
-    static int fib1 = 0;
-    static int fib2 = 1;
-    
+
     public static void main(String a[]){
-        System.out.println(fib1);
-        for(int i=0; i<15; i++) {
-            System.out.println(fib2);
+//        fibonacci works for small numbers
+        System.out.println("fibonacci: " + fibonacci(15));
+//        fibonacci 48 overflows, see the negative value returned?
+        System.out.println("fibonacci: " + fibonacci(49));
+//        replace ints with BigIntegers
+        System.out.println("fibonacciBigInteger: " + fibonacciBigInteger(6222));
+//        fibonacciRec is a recursive function, unfortunately the stack overflows
+        System.out.println("fibonacciRec: " + fibonacciRec(0, 6222, BigInteger.ZERO, BigInteger.ONE, BigInteger.ZERO));
+    }
+
+    public static int fibonacci(int val) {
+        int fib1 = 0;
+        int fib2 = 1;
+        for (int i = 0; i <= val; i++) {
+            if (i == 0) {
+                continue;
+            }
+            if (i == 1) {
+                continue;
+            }
             int aux = fib1 + fib2;
             fib1 = fib2;
             fib2 = aux;
         }
+        return fib2;
+    }
+
+    public static BigInteger fibonacciBigInteger(int val) {
+        BigInteger fib1 = BigInteger.ZERO;
+        BigInteger fib2 = BigInteger.ONE;
+        for (int i = 0; i <= val; i++) {
+            if (i == 0) {
+                continue;
+            }
+            if (i == 1) {
+                continue;
+            }
+            BigInteger aux = fib1.add(fib2);
+            fib1 = fib2;
+            fib2 = aux;
+        }
+        return fib2;
+    }
+
+    public static BigInteger fibonacciRec(int call, int calls, BigInteger fib1, BigInteger fib2, BigInteger result) {
+        if (call == calls)
+            return result;
+        if (call == 0) {
+            return fibonacciRec(++call, calls, fib1, fib2, BigInteger.ZERO);
+        } else if (call == 1) {
+            return fibonacciRec(++call, calls, fib1, fib2, BigInteger.ONE);
+        } else {
+            BigInteger aux = fib1.add(fib2);
+            fib1 = fib2;
+            fib2 = aux;
+        }
+        return fibonacciRec(++call, calls, fib1, fib2, fib1.add(fib2));
     }
 }
