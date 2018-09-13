8294bc3cfb90bb42083c25c71a177279b0720139
joelmatacv@runbox.com <G1thub@2012>
2018-09-13 04:46:16 +0200

    code refactoring

--- src/com/company/others/PrintXmasTreeDynamicCache.java
+++ src/com/company/others/PrintXmasTreeDynamicCache.java
@@ -7,18 +7,27 @@ import java.util.List;
 public class PrintXmasTreeDynamicCache {
 
     public static void main(String a[]) {
-        getXmasTree(3).stream().forEach(System.out::println);
+        getXmasTree(5).stream().forEach(System.out::println);
     }
 
     public static List<String> getXmasTree(int val) {
         List<String> cache = new ArrayList<>(val);
-        cache.add("*");
 
+//        calculate the biggest possible indentation at the root
+        String indentation = new String();
+        for (int i = 0; i < val - 1; i++) {
+            indentation = indentation.concat(" ");
+        }
+
+        cache.add("*");
         if (val == 1)
             return cache;
 
         for (int i = 1; i < val; i++) {
-            cache.add(cache.get(i - 1).concat("*"));
+            cache.add(cache.get(i - 1).concat("**"));
+            cache.set(i - 1, indentation.concat(cache.get(i - 1)));
+
+            indentation = indentation.substring(0, indentation.length() - 1);
         }
         return cache;
     }
