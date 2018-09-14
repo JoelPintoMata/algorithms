eaa93ca5052ec21fb57a2dc092d4479cfa7a4b68
joelmatacv@runbox.com <G1thub@2012>
2018-09-14 11:22:00 +0200

    anagram finder algorithm

---
+++ src/com/company/graph/IsAnagram.java
@@ -0,0 +1,24 @@
+package com.company.graph;
+
+
+/**
+ * Checks if a work is anagram of another
+ */
+public class IsAnagram {
+
+    public static void main(String[] args) {
+        System.out.println("Is joel anagram of noel?: " + isAnagram("joel", "noel"));
+        System.out.println("Is joel anagram of loje?: " + isAnagram("joel", "loje"));
+        System.out.println("Is joel 'funeral' of 'real fun'?: " + isAnagram("funeral", "real fun"));
+    }
+
+    private static boolean isAnagram(String from, String to) {
+        for (char c : to.toCharArray()) {
+            if (c == ' ')
+                continue;
+            if (from.indexOf(c) < 0)
+                return false;
+        }
+        return true;
+    }
+}
