484c6e5f7f8b57c9085f66a4e93077a9f63027cb
pintomataj <Elsevier1>
2018-11-27 10:21:33 +0100

    new algorithms

--- src/com/codility/fibonacci/FibFrog.java
+++ src/com/codility/fibonacci/FibFrog.java
@@ -1,6 +1,8 @@
 package com.codility.fibonacci;
 
-import java.util.Arrays;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 
 /**
@@ -10,20 +12,59 @@ import java.util.Arrays;
  */
 public class FibFrog {
 
+    private static int jumps = 0;
+    private static Queue queue = new PriorityQueue();
+
     public static void main(String a[]){
         System.out.println(solution(new int[]{0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0}));
     }
 
-    public static int[] solution(int[] A) {
-        int [] rotatedA = new int[A.length];
+    public static int solution(int[] A) {
+        solutionAux(A);
+        jumps++;
+        return jumps;
+    }
 
-        for(int i=0; i<A.length; i++) {
-            //rotated index needs to "wrap" around end of array
-            //the mode operator gives us just that 15 mod 10 == 5
-            int rotatedIndex = (i + K) % A.length;
+    public static void solutionAux(int[] A) {
+        if(A.length == 0)
+            return;
 
-            rotatedA[rotatedIndex] = A[i];
+        for(int i=0; i<A.length-1; i++) {
+            if(A[i]==0 && queue.isEmpty()) {
+                continue;
+            }
+            if(A[i]==0 && !queue.isEmpty()) {
+                System.out.println("jump to " + queue.poll());
+                ++jumps;
+                queue.clear();
+                continue;
+            }
+//            we reach the other side of the river
+            if(i==0) {
+                System.out.println("jump to " + i);
+                ++jumps;
+                return;
+            }
+//            lets skip these ones
+            if(A[i]==1) {
+                queue.add(i);
+                continue;
+            }
+            if(fib(A[i]) < A.length) {
+                System.out.println("jump to " + i);
+                ++jumps;
+                if(i==0)
+                    return;
+            }
         }
-        return rotatedA;
+        return;
+    }
+
+    private static int fib(int fib) {
+        if(fib==0)
+            return 0;
+        if(fib==1)
+            return 1;
+        return fib(fib-1) + fib(fib+1);
     }
 }
