56974a697973ed1ce1bc4f1dc9eec9dd41f90e07
joelmatacv@runbox.com <G1thub@2012>
2018-11-28 00:35:13 +0100

    code refactoring

--- src/com/codility/fibonacci/FibFrog.java
+++ src/com/codility/fibonacci/FibFrog.java
@@ -1,9 +1,9 @@
 package com.codility.fibonacci;
 
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Check: https://app.codility.com/programmers/lessons/2-arrays/
@@ -12,59 +12,77 @@ import java.util.Queue;
  */
 public class FibFrog {
 
+    private static List<Integer> fibs;
+    private static int min;
     private static int jumps = 0;
-    private static Queue queue = new PriorityQueue();
 
     public static void main(String a[]){
-        System.out.println(solution(new int[]{0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0}));
+
+        min = -1;
+        jumps = 0;
+        solution(new int[]{0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0});
+        System.out.println(min);
+
+        min = -1;
+        jumps = 0;
+        solution(new int[]{1, 1});
+        System.out.println(min);
+
+        min = -1;
+        jumps = 0;
+        solution(new int[]{0});
+        System.out.println(min);
+
+        min = -1;
+        jumps = 0;
+        solution(new int[]{});
+        System.out.println(min);
     }
 
     public static int solution(int[] A) {
-        solutionAux(A);
-        jumps++;
+        fibs = fibs(A);
+        solutionAux(A, fibs);
         return jumps;
     }
 
-    public static void solutionAux(int[] A) {
-        if(A.length == 0)
+    public static void solutionAux(int[] A, List<Integer> fibs) {
+        if (min != -1 && jumps > min) {
+            --jumps;
             return;
+        }
 
-        for(int i=0; i<A.length-1; i++) {
-            if(A[i]==0 && queue.isEmpty()) {
+        for (int i = fibs.size() - 1; i > 0; i--) {
+//            the jump is too big
+            if (fibs.get(i) > A.length)
                 continue;
-            }
-            if(A[i]==0 && !queue.isEmpty()) {
-                System.out.println("jump to " + queue.poll());
-                ++jumps;
-                queue.clear();
-                continue;
-            }
-//            we reach the other side of the river
-            if(i==0) {
-                System.out.println("jump to " + i);
-                ++jumps;
+
+            if (fibs.get(i) == A.length) {
+                jumps++;
+                if (min == -1 || jumps < min) {
+                    min = jumps;
+                }
+                --jumps;
                 return;
             }
-//            lets skip these ones
-            if(A[i]==1) {
-                queue.add(i);
-                continue;
-            }
-            if(fib(A[i]) < A.length) {
-                System.out.println("jump to " + i);
-                ++jumps;
-                if(i==0)
-                    return;
+
+            if (A[fibs.get(i) - 1] == 1) {
+                jumps++;
+                solutionAux(Arrays.copyOfRange(A, fibs.get(i) - 1, A.length), fibs);
             }
         }
         return;
     }
 
-    private static int fib(int fib) {
-        if(fib==0)
-            return 0;
-        if(fib==1)
-            return 1;
-        return fib(fib-1) + fib(fib+1);
+    private static List<Integer> fibs(int[] A) {
+        List<Integer> fibs = new ArrayList();
+        for (int i = 0; i < A.length * 2; i++) {
+            if (i == 0)
+                fibs.add(0);
+            else if (i == 1)
+                fibs.add(1);
+            else
+                fibs.add(fibs.get(i - 1) + fibs.get(i - 2));
+        }
+        return fibs;
     }
 }
